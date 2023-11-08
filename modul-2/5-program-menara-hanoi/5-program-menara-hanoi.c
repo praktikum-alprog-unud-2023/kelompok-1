1b1302a89cf607dd38d53366794b8b0483b1c034
Senjasripratiwi <senjasrip@gmail.com>
2023-11-08 06:19:06 -0800

    program menara hanoi iteratif

--- modul-2/5-program-menara-hanoi/5-program-menara-hanoi.c
+++ modul-2/5-program-menara-hanoi/5-program-menara-hanoi.c
@@ -1,50 +1,55 @@
 #include <stdio.h>
+#include <math.h>
 
-void hanoi_iteratif(int n, char sumber, char bantu, char tujuan) {
-    int stack[n * n][4];
-    int top = -1;
-    int langkah = 0;
-    stack[++top][0] = n;
-    stack[top][1] = sumber;
-    stack[top][2] = bantu;
-    stack[top][3] = tujuan;
+// Fungsi untuk menghitung jumlah langkah minimum
+int hitungLangkahMinimum(int n) {
+    return (int)(pow(2, n) - 1);
+}
 
-    while (top >= 0) {
-        int n = stack[top][0];
-        char sumber = stack[top][1];
-        char bantu = stack[top][2];
-        char tujuan = stack[top][3];
-        top--;
+// Fungsi untuk menyelesaikan masalah Menara Hanoi secara iteratif
+void hanoiIteratif(int n) {
+    int totalLangkah = hitungLangkahMinimum(n);
+    char tiang_sumber = 'A';
+    char tiang_tujuan = 'C';
+    char tiang_bantu = 'B';
 
-        if (n == 1) {
-            printf("Pindahkan cakram 1 dari %c ke %c\n", sumber, tujuan);
-            langkah++;
-        } else {
-            stack[++top][0] = n - 1;
-            stack[top][1] = bantu;
-            stack[top][2] = sumber;
-            stack[top][3] = tujuan;
-
-            stack[++top][0] = 1;
-            stack[top][1] = sumber;
-            stack[top][2] = bantu;
-            stack[top][3] = tujuan;
-
-            stack[++top][0] = n - 1;
-            stack[top][1] = sumber;
-            stack[top][2] = tujuan;
-            stack[top][3] = bantu;
-        }
+    if (n % 2 == 0) {
+        char sumber = tiang_tujuan;
+        tiang_tujuan = tiang_bantu;
+        tiang_bantu = sumber;
     }
 
-    printf("Jumlah langkah minimum: %d\n", langkah);
+    for (int langkah = 1; langkah <= totalLangkah; langkah++) {
+        if (langkah % 3 == 1) {
+            printf("Pindahkan cakram %d dari %c ke %c\n", langkah % 3, tiang_sumber, tiang_tujuan);
+        } else if (langkah % 3 == 2) {
+            printf("Pindahkan cakram %d dari %c ke %c\n", langkah % 3, tiang_sumber, tiang_bantu);
+        } else if (langkah % 3 == 0) {
+            printf("Pindahkan cakram %d dari %c ke %c\n", langkah % 3, tiang_bantu, tiang_tujuan);
+        }
+    }
 }
 
 int main() {
-    int n;
-    printf("Masukkan jumlah cakram: ");
-    scanf("%d", &n);
+    int n,validinput=0;
+    
+    //Fungsi untuk memasukkan nilai dan melakukan validasi
+    do{
+    printf ("Masukkan jumlah cakram : ");
+    if(scanf ("%d",&n) !=1){
+        printf ("Nilai harus berupa angka! Coba lagi \n");
+        while (getchar()!='\n');
+    } else {
+        validinput =1;
+    }
+    } while (!validinput);
+    validinput=0;
 
-    hanoi_iteratif(n, 'A', 'B', 'C');
+    int langkah_minimum = hitungLangkahMinimum(n);
+    printf("Jumlah langkah minimum : %d\n", langkah_minimum);
+
+    printf("Langkah penyelesaian : \n");
+    hanoiIteratif(n);
+
     return 0;
 }
